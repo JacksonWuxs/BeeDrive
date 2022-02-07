a293a31e85c86f73f238f486fc0e0a1c2bd10052
JacksonWuxs <wuxsmail@163.com>
2022-02-06 20:58:03 -0500

    V 0.2.0a1 (released)
    Now BeeDrive supports HTTP access files

--- BeeDrive/core/browser.py
+++ BeeDrive/core/browser.py
@@ -2,6 +2,8 @@ import pickle
 import time
 import os
 import itertools
+import socketserver
+import shutil
 
 
 from .base import BaseWaiter
@@ -12,10 +14,7 @@ from .utils import get_uuid
 WELCOME = "Welcome to BeeDrive Cloud Service!"
 LOGIN = """<h2> Please Login</h2><form action="/" method="GET" target="_self" autocomplete="on"><p> User Name: <input type="text" name="user"><br></p><p> &nbsp; &nbsp;Password: <input type="password" name="passwd"> <input type="submit" value="Login"> </p></form>"""
 SOURCE_DIR = os.path.split(os.path.split(__file__)[0])[0]
-with open(SOURCE_DIR + "/source/index.html", "r", encoding="utf8") as f:
-    INDEX_PAGE = f.read()
-with open(SOURCE_DIR + "/source/icon.ico", "rb") as f:
-    ICON = f.read()
+INDEX_PAGE = open(SOURCE_DIR + "/source/index.html", "r", encoding="utf8").read()
 
 
 class GetWaiter(BaseWaiter):
@@ -28,35 +27,38 @@ class GetWaiter(BaseWaiter):
 
     def run(self):
         with self:
-            print(self.token, self.task, self.user, self.passwd)
-            
             if self.task == "index":
-                status, ctype, content = "200", "text/html; charset=utf-8", INDEX_PAGE % (WELCOME, LOGIN)
+                self.response(INDEX_PAGE % (WELCOME, LOGIN))
             elif self.task == "login":
-                status, ctype, content = "200", "text/html; charset=utf-8", self.render_login()
+                self.response(self.render_login())
             elif self.task == "get":
                 if self.token == "/favicon.ico":
-                    status, ctype, content = "200", "application/octet-stream", ICON
+                    self.response(open(SOURCE_DIR + "/source/icon.ico", "rb"))
                 else:
-                    status, ctype, content = self.render_get()
-       
-            self.response(status, ctype, content)
-            print("Responsed!")
+                    self.response(self.render_get())
+            self.socket.close()
 
-    def response(self, status, ctype, content):
-        if not isinstance(content, bytes):
-            content = content.encode("utf8")
-        header = ["HTTP/1.1 %s OK" % status,
+    def response(self, content):
+        if isinstance(content, str):
+            content = content.encode("utf-8")
+        ctype = "text/html; charset=utf-8" if isinstance(content, bytes) else "application/octet-stream"
+        clength = len(content) if isinstance(content, bytes) else str(os.fstat(content.fileno())[6])
+        cdispos = "inline" if isinstance(content, bytes) else 'attachment; filename="%s"' % os.path.split(content.name)[-1]
+        header = ["HTTP/1.1 200 OK",
                   "Connection: close",
                   "Content-Type: %s" % ctype,
-                  "Content-Length: %d" % (8 * len(content)),
-                  #"Content-Disposition: attachment; filename=Test.pdf",
+                  "Content-Length: %s" % clength,
+                  'Content-Disposition: %s' % cdispos,
                   "Cache-Control: no-cache",
                   "\r\n"]
         header = "\r\n".join(header).encode("utf8")
+        self.socket.sendall(header)
+        if isinstance(content, bytes):
+            self.socket.sendall(content)
+        else:
+            writer = socketserver._SocketWriter(self.socket)
+            shutil.copyfileobj(content, writer)
         
-        self.socket.sendall(header + content.replace(b"\n", b"\r\n"))
-        self.socket.close()
 
     def render_login(self):
         if self.user not in self.userinfo:
@@ -65,39 +67,48 @@ class GetWaiter(BaseWaiter):
             return INDEX_PAGE % ("Password is incorrect!", LOGIN)
 
         token = get_uuid()
-        with open(os.path.join(self.root, "." + token), "wb") as f:
+        if not os.path.exists(os.path.join(self.root, "./.cookis")):
+            os.makedirs(os.path.join(self.root, "./.cookis"))
+        with open(os.path.join(self.root, "./.cookis/." + token), "wb") as f:
             pickle.dump({"user": self.user, "deadline": time.time() + 600}, f)
-        return INDEX_PAGE % ("Welcome back %s!" % self.user, self.render_list_dir(self.user, token))
+        return INDEX_PAGE % ("Hi %s, welcome back!" % self.user, self.render_list_dir(self.user, token))
 
     def render_get(self):
-        token_path = os.path.join(self.root, "." + self.user)
+        token_path = os.path.join(self.root, "./.cookis/." + self.user)
         if not os.path.exists(token_path):
-            return "200", "text/html", INDEX_PAGE % ("Cookie Token is expired!", LOGIN)
+            return INDEX_PAGE % ("Cookie is expired!", LOGIN)
         info = pickle.load(open(token_path, "rb"))
         if time.time() > info["deadline"]:
-            return "200", "text/html", INDEX_PAGE % ("Cookie Token is expired!", LOGIN)
+            os.remove(self.root, "./cookis/." + self.user)
+            return INDEX_PAGE % ("Cookie is expired!", LOGIN)
         
         self.user, self.passwd, query, token = info["user"], self.userinfo[info["user"]], self.passwd, self.user
         query = query.replace("%20", " ")
         target = os.path.join(self.root, query)
         if os.path.isdir(target):
-            page = INDEX_PAGE % ("Welcome back %s!" % self.user, self.render_list_dir(query, token))
-            return "200", "text/html", page
-        with open(target, "rb") as f:
-            return "200", "application/force-download", f.read()
+            return INDEX_PAGE % ("Hi %s, welcome back!" % self.user, self.render_list_dir(query, token))
+        return open(target, "rb")
 
     def render_list_dir(self, root, token):
-        content = "<h2>Directory listing for /%s </h2>" % root
+        content = "<h3>Visiting: /%s</h3>" % root
+        content += "<h3>Upload</h3>"
+        content += '<form method="post">'
+        content += '<input ref="input" multiple name="file" type="file"/>'
+        content += '<input type="submit" value="Upload"/></form>'
+        content += "<br>"
+        
+        content += "<h3>Download</h3>"
         content += "<ul>"
         father_root = os.path.split(os.path.abspath(root))[0]
         if root != self.user:
+            
             father_root = root[:-1] if root.endswith("/") else root
             father_root = os.path.split(father_root)[0]
             content += '<li><a href="/?cookie=%s&file=%s">../</a>' % (token, father_root)
 
         dirs, files = [], []
         for each in sorted(os.listdir(os.path.join(self.root, root))):
-            if os.path.isdir(os.path.join(self.root, each)):
+            if os.path.isdir(os.path.join(self.root, root, each)):
                 each += r"/"
                 dirs.append(each)
             else:
